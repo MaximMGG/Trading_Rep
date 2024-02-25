b3529ce6f6dccc346690eede299c9c25ffd20677
maximhrunenko <maxhrunenko@gmail.com>
2024-02-25 22:01:43 +0200

    bug in app(main) need to correct

--- manager/src/app.c
+++ manager/src/app.c
@@ -22,9 +22,11 @@ static struct Quote_args parse_args(char *arg) {
 }
 
 
+#ifdef TEST
 App_ *init_app(i32 argc, char **argv) {
     App_ *app = (App_ *) malloc(sizeof(App_));
     app->template = (Quotes **) malloc(sizeof(Quotes *) * argc - 1);
+    app->quotes_size = 0;
     for(int i = 1; i < argc; i++) {
         struct Quote_args q = parse_args(argv[i]);
         app->template[i] = Quotes_create(q.q_time, (int (*) (Quotes *)) q.strategy, newstr(q.q_ticker));
@@ -32,6 +34,15 @@ App_ *init_app(i32 argc, char **argv) {
     } 
     return app;
 }
+#else
+App_ *init_app() {
+    App_ *app = (App_ *) malloc(sizeof(App_));
+    app->template = (Quotes **) malloc(sizeof(Quotes *) * 1);
+    app->template[0] = Quotes_create(1, (i32 (*) (Quotes *))strategy_one_starter, newstr("BTCUSDT"));
+    app->quotes_size = 1;
+    return app;
+}
+#endif
 
 
 void close_app(App_ *app) {
@@ -41,14 +52,19 @@ void close_app(App_ *app) {
 }
 
 int main(i32 argc, char **argv) {
-    char abc[][10] = {"1", "aa", "BTCUSDT"};
+#ifdef TEST
     App_ *app = init_app(2, (char **)abc);
+#else
+    App_ *app = init_app();
+#endif
     data_init();
 
     while(true) {
         for(int i = 0; i < app->quotes_size; i++) {
             Res_ticker *res = send_get_ticker_request(app->template[i]->ticker->str);
             Candle *new = Candle_new(res->lastPrice, res->openTime, app->template[i]->t);
+            //TODO(Maxim) app->template[i]->candles is NULL for start app, need
+            //to correct that
             if (Candle_check_time(app->template[i]->candles[app->template[i]->candle_size - 1], new, app->template[i]->t) == NEW_CANDLE) {
                 Quotes_add_candle(app->template[i], new);
                 Quotes_action(app->template[i]);
